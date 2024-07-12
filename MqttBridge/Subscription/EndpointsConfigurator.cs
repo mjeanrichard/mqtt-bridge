949c3aa7a5aefb1c7577d375615a48d79ddccd90
Meinrad Jean-Richard <mjr@jean-richard.ch>
2024-07-12 21:25:41 +0200

    fix topic

--- MqttBridge/Subscription/EndpointsConfigurator.cs
+++ MqttBridge/Subscription/EndpointsConfigurator.cs
@@ -59,7 +59,7 @@ public class EndpointsConfigurator : IEndpointsConfigurator
                     .AddMqttInbound<EnvSensorInfoMessage>("devices/philoweg/+/info", "MB_EnvSensorInfo" + _mqttSettings.ClientSuffix, CreateCamelCaseSerializer<EnvSensorInfoMessage>())
                     .AddMqttInbound<EnvSensorMeasurement>("devices/philoweg/+/sensors/+", "MB_EnvSensorMeasurement" + _mqttSettings.ClientSuffix, CreateCamelCaseSerializer<EnvSensorMeasurement>())
                     .AddMqttInbound<GasMeterMessage>("devices/philoweg/gas/+", "MB_GasMeter" + _mqttSettings.ClientSuffix, CreateDefaultSerializer<GasMeterMessage>())
-                    .AddMqttInbound<MqttGatewayMessage>("devices/omgOpenMQTTGateway/LORAtoMQTT/+", "MB_MqttGateway" + _mqttSettings.ClientSuffix, CreateDefaultSerializer<MqttGatewayMessage>())
+                    .AddMqttInbound<MqttGatewayMessage>("devices/OMG_LILYGO/LORAtoMQTT/+", "MB_MqttGateway" + _mqttSettings.ClientSuffix, CreateDefaultSerializer<MqttGatewayMessage>())
             );
     }
 }
